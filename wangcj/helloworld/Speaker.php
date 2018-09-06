598c2f2b1f6cf494f3d8e2235c0c7538e4c8e04b
wangchaojie <wangchaojie@huoba.com>
2018-09-06 15:37:59 +0800

    Just a demo for composer

--- wangcj/helloworld/Speaker.php
+++ wangcj/helloworld/Speaker.php
@@ -12,7 +12,7 @@ namespace wangcj\helloworld;
 class Speaker {
 
     public function __construct(){
-        echo 'I\'m a speaker V0.3 !';
+        echo 'I\'m a speaker V1.0 !';
     }
     public function getVolume(){
         echo 'Speaker volume value is 54';

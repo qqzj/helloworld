bdc1d766ac737888a586b92ccd4ded5727adf89b
wangchaojie <wangchaojie@nodecap.com>
2018-09-07 12:27:11 +0800

    helloworld

--- wangcj/helloworld/helloworld.php
+++ wangcj/helloworld/helloworld.php
@@ -12,7 +12,7 @@ namespace wangcj\helloworld;
 class helloworld {
 
     public function __construct(){
-        echo 'I\'m a speaker V6.6 !';
+        echo 'I\'m a speaker V6.7 !';
     }
     public function getVolume(){
         echo 'Speaker volume value is 54';
